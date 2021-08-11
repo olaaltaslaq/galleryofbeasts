2056824edd74263cb4f12def2c4237aa94348745
root <root@LTUCSTUDENT-L154.localdomain>
2021-08-11 23:45:30 +0300

    lab 4

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
 import './App.css';
+// import FilterBeast from './Components/FilterBeast';
 
 class App extends React.Component {
   render(){
     return(
       <>
         <Header/>
+        {/* <FilterBeast FloatingLabel={this.porps.FloatingLabel}/> */}
         <Main/>
         <Footer/>
       </>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,14 +3,46 @@ import HornedBeast from "./HornedBeasts"
 import BeastsJson from "./BeastsJson";
 import './Main.css';
 
+
 class Main extends React.Component {
+  
+  // constructor (props){
+  //   super(props);
+  //     this.state={
+  //       data : this.props.BeastsJson ,
+  //     };
+
+  //   }
+  //   selectHorns = async (e) => {
+  //     e.preventDefault();
+  //     let filterdData = [];
+  //     let num =Number(e.target.value);
+  //     if (!(isNaN(num))) {
+  //       console.log(num);
+  //       filterdData = this.props.BeastsJson.filter((obj) => {
+  //         if (obj.horns === num) {
+  //           return 1;
+  //         }
+  //       });
+  //     } else {
+  //       console.log(num);
+  //       filterdData = this.props.BeastsJson ;
+  //     }
+  //     await this.setState({
+  //       data: filterdData,
+  //     });
+  //   };
+
     render() {
         return (
             <div>
+
         {
            BeastsJson.map((x,indx)=>{
             return(
+
               <HornedBeast  key={indx} title = {x.title} imageUrl = {x.image_url} description = {x.description} keyword = {x.keyword} horns = {x.horns}/>
+
             );
           })
         }
@@ -20,4 +52,3 @@ class Main extends React.Component {
 }
 
 export default Main;
-
