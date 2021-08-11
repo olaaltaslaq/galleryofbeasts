1cfabcd739aba7047d30a30f60e59e4a08f108af
root <root@LTUCSTUDENT-L154.localdomain>
2021-08-11 20:32:06 +0300

    add4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-// import './App.css';
+import './App.css';
 
 class App extends React.Component {
   render(){
